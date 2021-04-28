69ae9b91a77b61c73da8035d18c696c9b2b7a3ab
Kevin Kamani <kevinkamani17@gmail.com>
2021-04-28 15:16:14 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,5 @@
 #!/bin/#!/usr/bin/env bash
 
 
-sudo chown www-data /etc/apache2/sites-available/kimai.conf
-sudo a2dissite 000-default
-sudo a2ensite kimai
-sudo service apache2 restart
+cd /var/www/kimai2
+apache2-foreground
