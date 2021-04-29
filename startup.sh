e2bc1d27ead0956875997afd322861b35949365b
Kevin Kamani <kevinkamani17@gmail.com>
2021-04-29 20:54:30 +0200

    Updated startup.sh

--- startup.sh
+++ startup.sh
@@ -2,7 +2,14 @@
 
 
 cd /var/www/kimai2
+sed -i "s/%USERNAME%/$USERNAME/" .env
+sed -i "s/%PASSWORD%/$PASSWORD/" .env
+sed -i "s/%DB_NAME%/$DB_NAME/" .env
+sed -i "s/%DB_PORT%/$DB_PORT/" .env
+sed -i "s/%DB_HOST%/$DB_HOST/" .env
+echo "Installing Kimai..."
 bin/console kimai:install -n
+echo "Fixing permissions..."
 chown -R :www-data .
 chmod -R g+r .
 chmod -R g+rw var/
