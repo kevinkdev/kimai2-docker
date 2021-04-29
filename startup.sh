93c8e293394c1f78f1c2a55475bcf86688ecf122
Kevin Kamani <kevinkamani17@gmail.com>
2021-04-29 22:06:36 +0200

    updated startup.sh

--- startup.sh
+++ startup.sh
@@ -7,10 +7,9 @@ sed -i "s/%PASSWORD%/$PASSWORD/" .env
 sed -i "s/%DB_NAME%/$DB_NAME/" .env
 sed -i "s/%DB_PORT%/$DB_PORT/" .env
 sed -i "s/%DB_HOST%/$DB_HOST/" .env
-echo "Loading Kimai..."
 bin/console kimai:install -n
-chown -R :www-data .
-chmod -R g+r .
-chmod -R g+rw var/
-chmod -R g+rw public/avatars/
-apachectl-foreground
+sudo chown -R :www-data .
+sudo chmod -R g+r .
+sudo chmod -R g+rw var/
+sudo chmod -R g+rw public/avatars/
+apachectl start
