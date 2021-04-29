ebc9efd1af3799f6ba326f602616b998ccf31182
Kevin Kamani <kevinkamani17@gmail.com>
2021-04-29 21:22:52 +0200

    updated startup.sh

--- startup.sh
+++ startup.sh
@@ -7,9 +7,8 @@ sed -i "s/%PASSWORD%/$PASSWORD/" .env
 sed -i "s/%DB_NAME%/$DB_NAME/" .env
 sed -i "s/%DB_PORT%/$DB_PORT/" .env
 sed -i "s/%DB_HOST%/$DB_HOST/" .env
-echo "Installing Kimai..."
+echo "Loading Kimai..."
 bin/console kimai:install -n
-echo "Fixing permissions..."
 chown -R :www-data .
 chmod -R g+r .
 chmod -R g+rw var/
