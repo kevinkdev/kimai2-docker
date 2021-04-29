3a39c33925892e6c7e1c9794bf825ff3c30a167c
Kevin Kamani <kevinkamani17@gmail.com>
2021-04-29 22:12:34 +0200

    removed chown's calls

--- startup.sh
+++ startup.sh
@@ -8,8 +8,4 @@ sed -i "s/%DB_NAME%/$DB_NAME/" .env
 sed -i "s/%DB_PORT%/$DB_PORT/" .env
 sed -i "s/%DB_HOST%/$DB_HOST/" .env
 bin/console kimai:install -n
-sudo chown -R :www-data .
-sudo chmod -R g+r .
-sudo chmod -R g+rw var/
-sudo chmod -R g+rw public/avatars/
 apachectl start
